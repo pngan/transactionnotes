fa78312615f295c024acf0baaf66d9dfe909cced
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-02-07 21:35:18 +1300

    Moved the config variables out of the AppHost because this is not used in docker

--- transactionnotes.AppHost/Program.cs
+++ transactionnotes.AppHost/Program.cs
@@ -2,17 +2,10 @@ using Microsoft.Extensions.Hosting;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var authority = builder.Configuration["TransNotes:Authority"];
-var clientId = builder.Configuration["TransNotes:ClientId"];
-var clientSecret = builder.Configuration["TransNotes:ClientSecret"];
-
 var apiService = builder.AddProject<Projects.transactionnotes_ApiService>("apiservice");
 
 builder.AddProject<Projects.transactionnotes_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithEnvironment("TransNotes__Authority", authority)
-    .WithEnvironment("TransNotes__ClientId", clientId)
-    .WithEnvironment("TransNotes__ClientSecret", clientSecret)
     .WithReference(apiService)
     .WaitFor(apiService);
 
