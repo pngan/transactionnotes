0c2f787b7bb53f369777520e20fc49fa55d4da07
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-02-07 13:49:55 +1300

    Start of adding MS Aspire Keycloak

--- transactionnotes.AppHost/Program.cs
+++ transactionnotes.AppHost/Program.cs
@@ -1,9 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.transactionnotes_ApiService>("apiservice");
+var username = builder.AddParameter("keycloak-username");
+var password = builder.AddParameter("keycloak-password", secret: true);
+var keycloak = builder.AddKeycloak("keycloak", 8088,
+                        username,
+                        password)
+                      .WithDataVolume();
+
+var apiService = builder.AddProject<Projects.transactionnotes_ApiService>("apiservice")
+                        .WithReference(keycloak)
+                        .WaitFor(keycloak);
 
 builder.AddProject<Projects.transactionnotes_Web>("webfrontend")
     .WithExternalHttpEndpoints()
+    .WithReference(keycloak)
     .WithReference(apiService)
     .WaitFor(apiService);
 
