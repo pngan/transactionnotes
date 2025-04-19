9d7483ac7015dc1096c7f4b983b59b9801e3188d
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-04-19 21:04:19 +1200

    The postgres database is being created at startup.
    Next is to read the connection string created in the apphost project

--- transactionnotes.AppHost/Program.cs
+++ transactionnotes.AppHost/Program.cs
@@ -1,8 +1,36 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.Configuration;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.transactionnotes_ApiService>("apiservice");
+var centralDbUsername = builder.Configuration["Database:Central:Username"];
+var centralDbPassword = builder.Configuration["Database:Central:Password"];
+
+if (string.IsNullOrEmpty(centralDbUsername) || string.IsNullOrEmpty(centralDbPassword))
+{
+    throw new InvalidConfigurationException("Please set the Database:Central:Username and Database:Central:Password in the appsettings.json or environment variables.");
+}
+
+var databasecentralusername = builder.AddParameter("databasecentralusername", centralDbUsername, publishValueAsDefault:false, secret: true);
+var databasecentralpassword = builder.AddParameter("databasecentralpassword", centralDbPassword, publishValueAsDefault: false, secret: true);
+
+var centraldbserver = builder.AddPostgres(
+    "centraldbserver", databasecentralusername, databasecentralpassword, 15432)
+    .PublishAsConnectionString()
+    .WithPgWeb(pgWeb => pgWeb.WithHostPort(5050))
+    .WithPgAdmin(pgAdmin => pgAdmin.WithHostPort(5051));
+
+
+var centraldb = centraldbserver.AddDatabase("centraldb", "centraldb");
+
+var centralDbService = builder.AddProject<Projects.CentralDb>("centraldbservice")
+    .WithReference(centraldbserver)
+    .WithReference(centraldb)
+    .WaitFor(centraldb);
+
+var apiService = builder.AddProject<Projects.transactionnotes_ApiService>("apiservice")
+    .WithReference(centralDbService)
+    .WaitForCompletion(centralDbService);
 
 builder.AddProject<Projects.transactionnotes_Web>("webfrontend")
     .WithExternalHttpEndpoints()
---
+++ transactionnotes.Database/CentralDb/Program.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+
+namespace CentralDb;
+
+using Microsoft.Extensions.Configuration;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // Build the configuration
+        var configuration = new ConfigurationBuilder()
+            //.SetBasePath(Directory.GetCurrentDirectory()) // Set the base path for configuration files
+            //.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true) // Load appsettings.json
+            //.AddEnvironmentVariables() // Optionally add environment variables
+            .Build();
+
+
+        var connectionString = configuration.GetConnectionString("centraldbserver");
+
+        // Pass the configuration to the repository
+        var repo = new DataRepository(configuration);
+        Thread.Sleep(5000);
+        Console.WriteLine("Hello, World!");
+
+
+
+    }
+
+    public class DataRepository(IConfiguration config)
+    {
+        public async Task GetAsync(string id)
+        {
+            var connectionString = config.GetConnectionString("centraldbserver");
+
+            await using var connection = new Npgsql.NpgsqlConnection(connectionString);
+            await connection.OpenAsync();
+
+            var query = "SELECT 1";
+            var result = await connection.QueryFirstOrDefaultAsync<int>(query);
+
+            Console.WriteLine($"Query Result: {result}");
+        }
+    }
+}
