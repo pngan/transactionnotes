11e56be68fbad5ca448f84846d82a3a6862c69fd
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-04-26 11:42:05 +1200

    Update nuget packages

--- transactionnotes.AppHost/Program.cs
+++ transactionnotes.AppHost/Program.cs
@@ -36,5 +36,5 @@ builder.AddProject<Projects.transactionnotes_Web>("webfrontend")
     .WaitFor(apiService);
 
 //builder.AddProject<Projects.transactionnotes_Tests>("tests");
-
+builder.AddDockerComposePublisher();
 builder.Build().Run();
