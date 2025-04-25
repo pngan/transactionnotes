ab7e7e4b55cd0e3599acce52662add96ab2e2616
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-04-25 13:17:12 +1200

    Added Ping endpoint and integration test

---
+++ transactionnotes.ApiService/Controllers/PingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace transactionnotes.ApiService.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Pong";
+        }
+    }
+}
