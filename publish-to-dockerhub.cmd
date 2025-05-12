969edb45c626e2b2bfcac128775b3179f451a0eb
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-05-12 21:04:19 +1200

    Change docker image tag to use today's date instead of latest

--- publish-to-dockerhub.cmd
+++ publish-to-dockerhub.cmd
@@ -1,9 +1,20 @@
+@echo off
+setlocal
+
+REM Get today's date in YYYYMMDD format using PowerShell
+for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMdd"') do set DATE_TAG=%%i
+
 cd transactionnotes.AppHost
 aspirate generate --output-format compose
 docker login --username pngan
-docker tag webfrontend pngan/webfrontend:latest
-docker push pngan/webfrontend:latest
-docker tag apiservice pngan/apiservice:latest
-docker push pngan/apiservice:latest
-docker tag centraldb-migration pngan/centraldb-migration:latest
-docker push pngan/centraldb-migration:latest
+
+docker tag webfrontend pngan/webfrontend:%DATE_TAG%
+docker push pngan/webfrontend:%DATE_TAG%
+
+docker tag apiservice pngan/apiservice:%DATE_TAG%
+docker push pngan/apiservice:%DATE_TAG%
+
+docker tag centraldb-migration pngan/centraldb-migration:%DATE_TAG%
+docker push pngan/centraldb-migration:%DATE_TAG%
+
+endlocal
