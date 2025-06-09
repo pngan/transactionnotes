57a86b1a34aca6789459c90a49aff491241686fd
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-06-09 15:44:55 +1200

    Publish images with latest tag in addition to date tag

--- publish-to-dockerhub.cmd
+++ publish-to-dockerhub.cmd
@@ -8,12 +8,18 @@ cd transactionnotes.AppHost
 aspirate generate --output-format compose
 docker login --username pngan
 
+docker tag webfrontend pngan/webfrontend:latest
+docker push pngan/webfrontend:latest
 docker tag webfrontend pngan/webfrontend:%DATE_TAG%
 docker push pngan/webfrontend:%DATE_TAG%
 
+docker tag apiservice pngan/apiservice:latest
+docker push pngan/apiservice:latest
 docker tag apiservice pngan/apiservice:%DATE_TAG%
 docker push pngan/apiservice:%DATE_TAG%
 
+docker tag centraldb-migration pngan/centraldb-migration:latest
+docker push pngan/centraldb-migration:latest
 docker tag centraldb-migration pngan/centraldb-migration:%DATE_TAG%
 docker push pngan/centraldb-migration:%DATE_TAG%
 
