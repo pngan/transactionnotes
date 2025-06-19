1344c4e906c0982d0d9969187fd663aee995c21c
Phil Ngan <4557674+pngan@users.noreply.github.com>
2025-06-19 21:34:27 +1200

    Rename users to singular user

--- CentralModule/centraldb-migration/SqlScripts/SchemaOwner/020-DDL/20250504.sql
+++ CentralModule/centraldb-migration/SqlScripts/SchemaOwner/020-DDL/20250504.sql
@@ -1,7 +1,7 @@
-﻿CREATE TABLE IF NOT EXISTS central.users (
+﻿CREATE TABLE IF NOT EXISTS central.user (
     -- Unique identifier for the user using UUID (GUID)
     -- gen_random_uuid() generates a new UUID for each new row
-    users_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
 
     -- User's email address, must be unique and not null
     email VARCHAR(255) UNIQUE NOT NULL,
@@ -21,7 +21,7 @@
 );
 
 -- Optional: Create an index on the email column for faster lookups
-CREATE INDEX idx_users_email ON central.users (email);
+CREATE INDEX idx_user_email ON central.user (email);
 
 -- Function to update updated_at_utc column on update using utc timezone
 CREATE OR REPLACE FUNCTION update_updated_at_utc_column()
@@ -33,7 +33,7 @@ END;
 $$ language 'plpgsql';
 
 -- Example trigger to use the function
-CREATE TRIGGER update_users_updated_at_utc
-BEFORE UPDATE ON central.users
+CREATE TRIGGER update_user_updated_at_utc
+BEFORE UPDATE ON central.user
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at_utc_column();
